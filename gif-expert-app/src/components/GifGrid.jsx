9f70e3bb8686545e3bdb1d9aae3246b3ed16353d
MR. CHANGO <jordanchango.clearminds@gmail.com>
2025-03-07 07:57:40 -0500

    leaningg

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { GifItem } from "./GifItem";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 
@@ -13,6 +12,9 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h3>{category}</h3>
+            {
+                isLoading && (<h2 >Cargando...</h2>)
+            }
             <div className="card-grid">
                 {
                     images.map((image) => (
--- gif-expert-app/src/hooks/useFetchGifs.js
+++ gif-expert-app/src/hooks/useFetchGifs.js
@@ -1,11 +1,16 @@
+import { useEffect, useState } from "react";
+import {getGifs} from '../helpers/getGifs'
 
-export const useFetchGifs = () => {
+export const useFetchGifs = (category) => {
 
      const [images, setImages] = useState([]);
+     
+    const [isLoading, setIsLoading ]= useState(true);
     
         const getImages = async () => {
             const newImages = await getGifs(category)
             setImages(newImages)
+            setIsLoading(false);
         }
     
         useEffect(() => {
@@ -14,8 +19,8 @@ export const useFetchGifs = () => {
     
 
     return {
-        images: [],
-        isLoading: true
+        images,
+        isLoading
     }
 
 }
