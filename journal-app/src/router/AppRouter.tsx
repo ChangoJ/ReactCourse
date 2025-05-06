17997ba69da626dfec3eec5978a9613fdc29fd9b
MR. CHANGO <jordanchango.clearminds@gmail.com>
2025-05-06 07:43:01 -0500

    fuckckckkc

---
+++ journal-app/src/router/AppRouter.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom"
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+    <Route path="/" element={ }/>
+    <Route path="/about" element={} />
+    {/* Agrega más rutas aquí según tus necesidades */}
+  </Routes>
+  )
+}
